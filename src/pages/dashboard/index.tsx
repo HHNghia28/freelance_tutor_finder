0a99668b706c43b1a4c1dde62484fcf3dd165e96
Huỳnh Hữu Nghĩa <nghiahh.work@gmail.com>
2024-10-23 22:06:11 +0700

    Update: Router

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,7 +4,7 @@ import { OverviewAnalyticsView } from 'src/sections/overview/analytics/view';
 
 // ----------------------------------------------------------------------
 
-const metadata = { title: `Thống kê chung` };
+const metadata = { title: `Tutor Finder` };
 
 export default function OverviewAppPage() {
   return (
