de77358cec1c62aeb7e0ce4f881c486009547dcd
Qhung3217 <quanghung165200@gmail.com>
2024-11-10 20:04:42 +0700

    fix eslint

--- src/actions/feedback.ts
+++ src/actions/feedback.ts
@@ -2,11 +2,11 @@
 
 import axios, { endpoints } from 'src/utils/axios';
 
-const swrOptions = {
-  revalidateIfStale: true,
-  revalidateOnFocus: false,
-  revalidateOnReconnect: false,
-};
+// const swrOptions = {
+//   revalidateIfStale: true,
+//   revalidateOnFocus: false,
+//   revalidateOnReconnect: false,
+// };
 
 const ENDPOINT = endpoints.feedback;
 
