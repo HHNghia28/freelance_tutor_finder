7601a8d2e1abef5cf88b7198e9fc01015f8f37ea
Qhung3217 <quanghung165200@gmail.com>
2024-11-14 22:09:17 +0700

    improve payment list

--- src/sections/_admin/payment/table/columns.tsx
+++ src/sections/_admin/payment/table/columns.tsx
@@ -1,10 +1,25 @@
 import type { IPayment } from 'src/types/payment';
 import type { GridColDef } from '@mui/x-data-grid';
 
+import { Box, Typography } from '@mui/material';
+
 import { fDateTime } from 'src/utils/format-time';
 import { fPercent, fCurrency } from 'src/utils/format-number';
 
 export const baseColumns: GridColDef<IPayment>[] = [
+  {
+    field: 'tutorAdvertisementsTitle',
+    flex: 1,
+    headerName: 'Bài đăng',
+    renderCell: (params) => (
+      <Box sx={{ width: 1 }}>
+        <Typography>{params.value}</Typography>
+        <Typography variant="caption" sx={{ color: 'text.secondary' }}>
+          {params.row.tutorName}
+        </Typography>
+      </Box>
+    ),
+  },
   {
     field: 'invoiceAmount',
 
